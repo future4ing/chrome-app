535609a5a99527098a996dadadc9e3767faa6175
future4ing <mirae4120@gmail.com>
2024-01-17 22:25:24 +0900

    TO DO LIST : 투두 저장하는 기능 추가

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() { // toDos의 내용을 로컬스토리지에 저장
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newTodo);
+    toDos.push(newTodo); // 배열 toDos에 newTodo를 넣음
+    paintToDo(newTodo); // 화면에 newTodo를 그려줌
+    saveToDos(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
